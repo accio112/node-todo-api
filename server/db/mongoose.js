15d430f249cc4e6e734c6e365ca828a131f6e0aa
Divya <accio112>
2018-10-22 09:29:02 +0530

    second commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise=global.Promise; //we are using the built in promise
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports={
 	mongoose
