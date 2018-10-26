c9044b81f429e3722bea23eee4c6826cc8993ded
Divya <accio112>
2018-10-27 00:46:03 +0530

    Hashed password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,32 @@
 const {SHA256} = require('crypto-js'); 
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data={
-	id:10
-};
+
+var password = '123abc!';
+
+//10 rounds
+// bcrypt.genSalt(10, (err, salt)=>{
+// 	bcrypt.hash(password, salt, (err, hash)=>{
+// 		console.log(hash);
+// 	})
+// });
+
+var hashedPassword = '$2a$10$oraIguFLx1nx3NLhtUo1tOMjc4Ue0z4RhAYKXuvrSJKs4ZLfEBp8m';
+
+
+bcrypt.compare('123', hashedPassword, (err, res)=>{
+	console.log(res);
+});
+// var data={
+// 	id:10
+// };
 
 //two methods- jwt.sign and jwt.verify
-var token = jwt.sign(data, '123abc'); //the value that will be sent to user once they sign in
-var decoded = jwt.verify(token, '123abc');
-console.log(token);
-console.log(decoded);
+// var token = jwt.sign(data, '123abc'); //the value that will be sent to user once they sign in
+// var decoded = jwt.verify(token, '123abc');
+// console.log(token);
+// console.log(decoded);
 
 
 
